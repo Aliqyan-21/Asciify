07b3cabae71164e63f87b20dfbff805484a0611f
Aliqyan-21 <aliqyanabid21@gmail.com>
2024-10-02 13:13:11 +0530

    feat: grayscaling the image

--- main.cpp
+++ main.cpp
@@ -1,16 +1,67 @@
 #include <iostream>
 #include <kalam.h>
+#include <vector>
 
 const std::string ascii_chars = "@%#*+=-:. ";
 
+unsigned char adjustContrast(unsigned char value, float contrast);
+
+char mapToAscii(unsigned char brightness);
+
+std::vector<unsigned char> rgbToGray(const kalam::Image &img, float contrast) {
+  int width = img.getWidth();
+  int height = img.getHeight();
+  int channels = img.getChannels();
+  const unsigned char *data = img.getData();
+
+  std::vector<unsigned char> grayScaleImage(width * height);
+
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < height; j++) {
+      int index = (j * width + i) * channels;
+      unsigned char r = data[index];
+      unsigned char g = data[index + 1];
+      unsigned char b = data[index + 2];
+
+      // weighted sum method
+      unsigned char grayscale =
+          static_cast<unsigned char>(0.299 * r + 0.587 * g + 0.114 * b);
+
+      // make grayscale image
+      grayScaleImage[j * width + i] = adjustContrast(grayscale, contrast);
+    }
+  }
+  return grayScaleImage;
+}
+
+void generateAscii(const kalam::Image &img, float contrast) {
+  // accessing image data with kalam funcs
+  int width = img.getWidth();
+  int height = img.getHeight();
+
+  std::vector<unsigned char> grayImage = rgbToGray(img, contrast);
+}
+
 int main(int argc, char *argv[]) {
   if (argc != 2) {
-    std::cerr << "Usage: " << argv[0] << " <image_file>" << std::endl;
+    std::cerr << "Usage: " << argv[0] << "<image_file>" << std::endl;
     exit(1);
   }
 
-  std::string imageFile = argv[1];
-
   // this is how we load image object in kalam
+  std::string imageFile = argv[1];
   kalam::Image img;
+
+  // loading
+  if (!img.load(imageFile) && !img.load(imageFile)) {
+    std::cerr << "Error loading image." << std::endl;
+    exit(1);
+  }
+
+  float contrast;
+  argc > 2 ? contrast = std::stof(argv[2]) : contrast = 1.5;
+
+  generateAscii(img, contrast);
+
+  return 0;
 }
