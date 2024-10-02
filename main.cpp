22b6d5f803b1ed9ee06a25a626062fa8b80f9e78
Aliqyan-21 <aliqyanabid21@gmail.com>
2024-10-02 13:15:54 +0530

    feat: adjusting contrast

--- main.cpp
+++ main.cpp
@@ -4,7 +4,10 @@
 
 const std::string ascii_chars = "@%#*+=-:. ";
 
-unsigned char adjustContrast(unsigned char value, float contrast);
+unsigned char adjustContrast(unsigned char value, float contrast) {
+  double new_value = ((((value / 255.0) - 0.5) * contrast) + 0.5) * 255;
+  (new_value < 0) ? 0 : (new_value > 255) ? 255 : new_value;
+}
 
 char mapToAscii(unsigned char brightness);
 
